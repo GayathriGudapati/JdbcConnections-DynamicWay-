b9180e660668af57888df85735d1d6bd92d027f0
BALA GAYATHRI GUDAPATI <152675023+GayathriGudapati@users.noreply.github.com>
2024-05-31 12:52:40 +0530

    jdbc connections

---
+++ demo/deletedatabase.java
@@ -0,0 +1,58 @@
+package demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class deletedatabase {
+	
+	public static final String Driver = "com.mysql.cj.jdbc.Driver";
+	public static final String Username = "root";
+	public static final String Password = "root";
+	public static final String Url = "jdbc:mysql://localhost:3306/";
+	public static Connection conn;
+	public static PreparedStatement pmst;
+
+	public static void main(String[] args) {
+		
+//		1.Driver
+//		2.create connection
+//		3.statement creation
+//		4.statement execution
+//		5.close
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Enter database name");
+			
+			String Url = "jdbc:mysql://localhost:3306/"+sc.next();
+			
+			Class.forName(Driver);
+			
+			Connection conn = DriverManager.getConnection(Url, Username, Password);
+			
+			System.out.println("Enter database name");
+			
+			String sql = "drop database "+sc.next();
+			
+			pmst = conn.prepareStatement(sql);
+			
+			int i = pmst.executeUpdate();
+			
+			if (i==0) {
+				System.out.println("deleted successfully");
+			}
+			else {
+				System.out.println("Error");
+			}
+			
+			conn.close();
+			pmst.close();
+			sc.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ demo/getall.java
@@ -0,0 +1,57 @@
+package demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class getall {
+	
+	public static final String Driver = "com.mysql.cj.jdbc.Driver";
+	public static final String Username = "root";
+	public static final String Password = "root";
+	public static final String Url = "jdbc:mysql://localhost:3306/";
+	public static Connection conn;
+	public static PreparedStatement pmst;
+
+	public static void main(String[] args) {
+		
+//		1.Driver
+//		2.create connection
+//		3.statement creation
+//		4.statement execution
+//		5.close
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Enter database name");
+			
+			String Url = "jdbc:mysql://localhost:3306/"+sc.next();
+			
+			Class.forName(Driver);
+			
+			Connection conn = DriverManager.getConnection(Url, Username, Password);
+			
+			System.out.println("Enter table name");
+			
+			String sql = "select * from "+sc.next();
+			
+			pmst = conn.prepareStatement(sql);
+		
+			ResultSet rs = pmst.executeQuery();
+			
+			while (rs.next()) {
+				System.out.println("id: " + rs.getInt("id")); 
+				System.out.println("name: " + rs.getString("name"));
+				System.out.println("email: " + rs.getString("email"));
+			}
+			conn.close();
+			pmst.close();
+			sc.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
